1c03d36405ba906b574143657ac3287ca78c35ca
shadow <namespaceshadow@gmail.com>
2018-10-06 19:49:19 +0800

    init

---
+++ foo.hpp
@@ -0,0 +1,14 @@
+//
+//  foo.hpp
+//  pku
+//
+//  Created by Shadow on 2018/8/29.
+//  Copyright © 2018年 Shadow. All rights reserved.
+//
+
+#ifndef foo_hpp
+#define foo_hpp
+
+#include <stdio.h>
+
+#endif /* foo_hpp */
