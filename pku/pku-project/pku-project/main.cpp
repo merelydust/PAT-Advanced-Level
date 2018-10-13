f4f945a03d6056fbd9dc4c0b770e1d4e9bcff337
shadow <namespaceshadow@gmail.com>
2018-10-13 22:09:59 +0800

    第三周树上

--- pku/pku-project/pku-project/main.cpp
+++ pku/pku-project/pku-project/main.cpp
@@ -1,49 +1,135 @@
+/***
+ 函数参数为指针type* ptr：      函数体内改变指针所指对象
+ 函数参数为指针引用type* &ptr：  函数体内改变指针所指方向
+ 
+ nodep就是一棵树 相当于树节点数组的名字 指向树节点数组的指针
+ ***/
+
 #include <iostream>
-#include <map>
+#include <string>
 using namespace std;
 
+struct record {
+    int left, right;
+};
+
+struct node {
+    int num; // 树节点在数组里的序号
+    node* left; // 左子树
+    node* right; // 右子树
+    //这样子定义 就可以实现 NodePrt->left->left...
+};
+
+struct queNode { //用队列存储树
+    node* tr; // 存储一棵(子)树
+    queNode* next; // 指向下一个队列节点
+};
+
+void inque(node* tr, queNode* &front, queNode* &rear) { // 入队函数
+    if (rear) { // 增加在尾
+        rear->next = new queNode;
+        rear = rear->next;
+        rear->tr = tr;
+        rear->next = nullptr;
+    }
+    else {
+        front = new queNode;
+        rear = front;
+        rear->tr = tr;
+        rear->next = nullptr;
+    }
+}
+
+void deque(queNode* &front, queNode* &rear) { //出队函数
+    if (front) { // 取删在头
+        if (front->next) {
+            queNode* tmp = front->next;
+            delete front;
+            front = tmp;
+        }
+        else {
+            delete front;
+            front = nullptr;
+            rear = nullptr;
+        }
+    }
+}
+
+void buildTree(record* rec, int root, node* &nodep) { //rec查询s是否有孩子 root是根节点序号 nodep是树
+    nodep = new node; // 新建一棵子树
+    nodep->num = root; //子树的根在数组里的序号等于root
+                       // 递归出口是 子树的根 其实只是个叶子
+    nodep->left = nullptr; // 子树初始化没有叶子
+    nodep->right = nullptr;
+    if (rec[root].left != -1) { // 如果rec中的root号树节点有左孩子
+        nodep->left = new node; // 以这个左孩子为根新建一棵子树
+        buildTree(rec, rec[root].left, nodep->left);
+    }
+    else {
+        nodep->left = nullptr;
+    }
+    if (rec[root].right != -1) { // 右边同理
+        nodep->right = new node;
+        buildTree(rec, rec[root].right, nodep->right);
+    }
+    else {
+        nodep->right = nullptr;
+    }
+}
+
+void printLeaves(node* nodep) {
+    if (!nodep) return;
+    queNode* front = nullptr; // 新建队列
+    queNode* rear = nullptr;
+    inque(nodep, front, rear); // 队列存储对象是各子树 从大到小
+    
+    bool isFirst = true;
+    while (front) {
+        if (!front->tr->left && !front->tr->right) { // 如果队列里->这棵树->没有左右孩子
+            if (isFirst) {
+                cout << front->tr->num;
+                isFirst = false;
+            }
+            else cout << ' ' << front->tr->num;
+        }
+        if (front->tr->left) inque(front->tr->left, front, rear); //把下一等级的的左子树插入队列
+        if (front->tr->right) inque(front->tr->right, front, rear); // 把下一等级的右子树插入队列
+        deque(front, rear); // 当前这一棵较高级子树出队
+    }
+}
+
 int main() {
-    map<int, int> poly;
-    map<int, int> m1;
-    map<int, int> m2;
-    int ex, co;
-    int n1, n2;
-    cin >> n1;
-    while (n1--) {
-        scanf("%d %d", &ex, &co);
-        poly[ex] = co;
-    }
-    cin >> n2;
-    while (n2--) {
-        scanf("%d %d", &ex, &co);
-        map<int, int>::iterator it;
-        for (it = poly.begin(); it != poly.end(); it++) {
-            m1[it->first + ex] = it->second + co;
+    int n; cin >> n;
+    record rec[10];
+    int check[10] = {0};
+    for (int i = 0; i < n; ++i) {
+        char tmp1, tmp2; cin >> tmp1 >> tmp2;
+        if (tmp1 != '-') {
+            rec[i].left = tmp1 - '0';
+            check[rec[i].left] = 1;
+        } else {
+            rec[i].left = -1;
         }
-        m2[ex] += co;
-    }
-    map<int, int>::iterator it1;
-    int tmpcnt = 0;
-    for (it1 = m1.begin(); it1 != m1.end(); it1++) {
-        if (tmpcnt) cout << ' ';
-        if (it1->second) {
-            cout << it1->first << ' ' << it1->second;
-            tmpcnt++;
+        if (tmp2 != '-') {
+            rec[i].right = tmp2 - '0';
+            check[rec[i].right] = 1;
+        } else {
+            rec[i].right = -1;
         }
     }
-    if (!tmpcnt) cout << "0 0";
-    cout << endl;
-    map<int, int>::iterator it2;
-    tmpcnt = 0;
-    for (it2 = m2.begin(); it2 != m2.end(); it2++) {
-        if (tmpcnt) cout << ' ';
-         if (it1->second) {
-             cout << it2->first << ' ' << it2->second;
-             tmpcnt++;
-         }
-        if (!tmpcnt) cout << "0 0";
-        cout << endl;
+    
+    int root = -1;
+    for (int i = 0; i < n; ++i) {
+        if (!check[i]) {
+            root = i;
+            break;
+        }
     }
+    if (root == -1) return 0;
+    node* nodep = nullptr;
+    buildTree(rec, root, nodep);
+    printLeaves(nodep);
+    
     return 0;
 }
 
---
+++ 浙大数据结构/第三周_树的同构.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+struct node {
+    char element;
+    int left, right;
+}tree1[10], tree2[10]; // 用静态链表存储树
+
+int buildTree(node* tree) { // 返回根节点在静态链表中的索引
+    int num; cin >> num;
+    if (!num) return -1;
+    int check[10] = {0};
+    for (int i = 0; i < num; ++i) {
+        char tmp1, tmp2;
+        cin >> tree[i].element >> tmp1 >> tmp2;
+        if (tmp1 != '-') {
+            tree[i].left = tmp1 - '0';
+            check[tree[i].left] = 1;
+        } else tree[i].left = -1;
+        if (tmp2 != '-') {
+            tree[i].right = tmp2 - '0';
+            check[tree[i].right] = 1;
+        } else tree[i].right = -1;
+    }
+    for (int i = 0; i < num; ++i) {
+        if (!check[i]) return i;
+    }
+    return -1;
+}
+
+bool isSame(int root1, int root2) {
+    if (root1 == -1 && root2 == -1) return true; // 两棵树都是空的
+    if (root1 == -1 || root2 == -1) return false; // 只有一棵树是空的
+    if (tree1[root1].element != tree2[root2].element) return false; // 树1根节点值不等于树2根节点值
+    // tree1[root1].left是下一个要判断的新根在静态链表中的索引
+    if (tree1[tree1[root1].left].element == tree2[tree2[root2].left].element) return isSame(tree1[root1].right, tree2[root2].right);
+    // 如果树1的左半边不等于树2的左半边 则左右交换比较 树1左对应树2右 树1右对应树1左
+    else return isSame(tree1[root1].left, tree2[root2].right) && isSame(tree1[root1].right, tree2[root2].left);
+}
+
+int main() {
+    int root1 = buildTree(tree1);
+    int root2 = buildTree(tree2);
+    if (isSame(root1, root2)) cout << "Yes" << endl;
+    else cout << "No" << endl;
+    return 0;
+}
