6d7c173b33e2318445fca648b462f76a925416d1
Rogary <rogaryljw@gmail.com>
2018-12-02 15:27:06 +0800

    Update A1052_Linked List Sorting.cpp

--- 算法笔记/A1052_Linked List Sorting.cpp
+++ 算法笔记/A1052_Linked List Sorting.cpp
@@ -8,31 +8,37 @@
 #include <algorithm>
 using namespace std;
 
-const int maxn = 100010;
+const int maxn = 10001;
 struct node {
     int address, data, next;
-}nodes[maxn];
-
+};
+vector<node> nodes;
 bool cmp(node a, node b) {
     return a.data < b.data;
 }
 
 int main() {
     int n, head; scanf("%d %d", &n, &head);
-    node tmp;
+    nodes.resize(200002);
     while(n--) {
+    	node tmp;
         scanf("%d %d %d", &tmp.address, &tmp.data, &tmp.next);
         nodes[tmp.address] = tmp;
     }
     vector<node> validNodes; node headNode = nodes[head];
     while (headNode.address != -1) {
-        validNodes.push_back(headNode); headNode = nodes[headNode.next];
+        validNodes.push_back(headNode); 
+		if(headNode.next==-1){
+			break;
+		}
+		headNode = nodes[headNode.next];
     }
     sort(validNodes.begin(), validNodes.end(), cmp);
-    if (validNodes.empty()) {
+	
+    if (validNodes.empty()||head==-1) {
         printf("0 -1\n"); return 0;
     }
-    printf("%d %d\n", validNodes.size(), validNodes[0].address);
+    printf("%d %05d\n", validNodes.size(), validNodes[0].address);
     for (int i = 0, len = validNodes.size(); i < len; ++i) {
         printf("%05d %d ", validNodes[i].address, validNodes[i].data);
         if (validNodes[i].next != -1) printf("%05d\n", validNodes[i].next);
