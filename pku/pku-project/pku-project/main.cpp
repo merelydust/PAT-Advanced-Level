9f0a51703c08618f7f161a426a34abbd5c440d79
shadow <namespaceshadow@gmail.com>
2018-12-04 21:39:40 +0800

    update

--- pku/pku-project/pku-project/main.cpp
+++ pku/pku-project/pku-project/main.cpp
@@ -1,30 +1,40 @@
 #include <cstdio>
-#include <cstring>
-#include <vector>
+#include <queue>
 #include <algorithm>
 using namespace std;
 
-const int maxn = 510;
-const int INF = 10 << 9;
+const int maxn = 10010;
+struct node {
+    int hh, mm, ss;
+    int ptime, wtime;
+    node() {
+        wtime = 0;
+    }
+}nodes[maxn];
 
-// n为顶点数 m为边数 Cmax为最大容量 sp为问题站点
-// G为邻接矩阵 weight为点权 d[]记录最短距离
-// minNeed记录最少携带数目 minRemain记录最少带回数目
-int n, m, Cmax, sp, numPath = 0, G[maxn][maxn], weight[maxn];
-int d[maxn], minNeed = INF, minRemain = INF;
-bool vis[maxn] = {false};
-vector<int> pre;
-vector<int> tmpPath, path; // 临时路径和最优路径
+bool cmp(node a, node b) {
+    if (a.hh != b.hh) return a.hh < b.hh;
+    else if (a.mm < b.mm) return a.mm < b.mm;
+    else return a.ss < b.ss;
+}
 
-void Dijkstra(int s) {
-    fill(d, d+maxn, INF);
-    d[s] = 0;
+
+int main() {
+    int n, k; scanf("%d %d", &n, &k);
+    node tmp;
     for (int i = 0; i < n; ++i) {
-        int u = -1, MIN = INF;
-        for (int j = 0; i < n; ++i) {
-            if (!vis[j] && d[j] < MIN) {
-                u = j; MIN = d[j];
-            }
+        scanf("%d:%d:%d %d", &tmp.hh, &tmp.mm, &tmp.ss, &tmp.ptime);
+        if (tmp.hh >= 17) continue;
+        else if (tmp.hh < 8) {
+            
         }
+        nodes[i] = tmp;
+    }
+    sort(nodes, nodes+n, cmp);
+    queue<node> Q;
+    for (int i = 0; i < 3; ++i) Q.push(nodes[i]); // 刚开门最前面三个人一起去
+    int ocp = 3; // 当前被占用窗口数
+    while (!Q.empty()) {
+        
     }
 }
