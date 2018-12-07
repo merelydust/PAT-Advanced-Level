0aa77eea93473a60509c8931aba5301ad3c45552
shadow <namespaceshadow@gmail.com>
2018-12-07 10:23:36 +0800

    2018/12/6

--- 真题模拟/A1143_Lowest Common Ancestor.cpp
+++ 真题模拟/A1143_Lowest Common Ancestor.cpp
@@ -1,55 +1,29 @@
 #include <cstdio>
+#include <map>
+using namespace std;
+
+const int maxn = 10010;
+int pre[maxn];
+// 开一个一万大小的哈希表有两个测试点过不了
+map<int, bool> hashtable; // 没有给定key取值范围的题目还是用map标记保险呀～
 
 int main() {
-    const int maxn = 10010;
     int m, n; scanf("%d %d", &m, &n);
-    int hashtable[maxn] = {0}; // 标记某个key是否出现
-    int pre[maxn];
-    for (int i =0; i <n; ++i) {
+    for (int i = 0; i < n; ++i) {
         scanf("%d", &pre[i]); hashtable[pre[i]] = 1;
     }
-    int u, v, k;
-    for (int i = 0; i < m; ++i) {
+    int u, v;
+    while (m--) {
         scanf("%d %d", &u, &v);
-        for(int j = 0; j < n; ++j) {
-            k = pre[j];
-            if ((k >= u && k <= v) || (k >= v && k <= u)) break;
+        int a = -1; // 祖先的值
+        for (int i = 0; i < n; ++i) {
+            a = pre[i];
+            if ((a >= u && a <= v) || (a >= v && a <= u)) break;
         }
         if (!hashtable[u] && !hashtable[v]) printf("ERROR: %d and %d are not found.\n", u, v);
-        else if (!hashtable[u] || !hashtable[v]) printf("ERROR: %d is not found.\n", hashtable[u] == 0 ? u : v);
-        else if (k == u || k == v) printf("%d is an ancestor of %d.\n", k, k == u ? v : u);
-        else printf("LCA of %d and %d is %d.\n", u, v, k);
+        else if (!hashtable[u] || !hashtable[v]) printf("ERROR: %d is not found.\n", hashtable[u]?v:u);
+        else if (a == u || a == v) printf("%d is an ancestor of %d.\n", a, a==u?v:u);
+        else printf("LCA of %d and %d is %d.\n", u, v, a);
     }
     return 0;
 }
-
-//#include <iostream>
-//#include <vector>
-//#include <map>
-//using namespace std;
-//map<int, bool> mp;
-//int main() {
-//    int m, n, u, v, a;
-//    scanf("%d %d", &m, &n);
-//    vector<int> pre(n);
-//    for (int i = 0; i < n; i++) {
-//        scanf("%d", &pre[i]);
-//        mp[pre[i]] = true;
-//    }
-//    for (int i = 0; i < m; i++) {
-//        scanf("%d %d", &u, &v);
-//        for(int j = 0; j < n; j++) {
-//            a = pre[j];
-//            if ((a >= u && a <= v) || (a >= v && a <= u)) break;
-//        }
-//        if (mp[u] == false && mp[v] == false)
-//            printf("ERROR: %d and %d are not found.\n", u, v);
-//        else if (mp[u] == false || mp[v] == false)
-//            printf("ERROR: %d is not found.\n", mp[u] == false ? u : v);
-//        else if (a == u || a == v)
-//            printf("%d is an ancestor of %d.\n", a, a == u ? v : u);
-//        else
-//            printf("LCA of %d and %d is %d.\n", u, v, a);
-//    }
-//    return 0;
-//}
