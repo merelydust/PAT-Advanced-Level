072cc3f7da9537ad0e2971975637bb320e5c1773
shadow <namespaceshadow@gmail.com>
2018-12-22 23:02:42 +0800

    update

--- 北大程序设计与算法专项课程/算法基础/深搜_A Knight's Journey.cpp
+++ 北大程序设计与算法专项课程/算法基础/深搜_A Knight's Journey.cpp
@@ -1,23 +1,36 @@
-//STATUS:C++_AC_16MS_168K
-#include<stdio.h>
-#include<memory.h>
-const int MAXN=10;
-int DFS(int tot,int rear,int x,int y);
-int p,q,way[MAXN*MAXN][2]={0},vis[MAXN][MAXN];
-int dx[8]={-2,-2,-1,-1,1,1,2,2},   //按字典序方向行走
-dy[8]={-1,1,-2,2,-2,2,-1,1};
-int main()
-{
-    //    freopen("in.txt","r",stdin);
-    int n;
-    scanf("%d",&n);
+#include <cstdio>
+#include <cstring>
+using namespace std;
+
+const int maxn = 10;
+int p, q, way[maxn*maxn][2] = {0}, vis[maxn][maxn] = {0};
+int dx[8]={-2,-2,-1,-1,1,1,2,2}, dy[8]={-1,1,-2,2,-2,2,-1,1}; // 8种走法
+
+int dfs(int tot, int rear, int x, int y) {
+    if (rear == tot) return 1;
+    else for(int i=0;i<8;i++){
+        int nx=x+dx[i],ny=y+dy[i];
+        if(nx>=0&&nx<q && ny>=0&&ny<p && !vis[nx][ny]){
+            vis[nx][ny]=1;      //标记为经过
+            if(dfs(tot,rear+1,nx,ny)){     //搜索下一个
+                way[rear][0]=nx; way[rear][1]=ny;
+                return 1;
+            }
+            vis[nx][ny]=0;   //搜索不成功，标记为未经过
+        }
+    }
+    return 0;   //搜索不成功
+}
+
+int main() {
+    int n; scanf("%d", &n);
     for(int i=1;i<=n;i++)
     {
         memset(vis,0,sizeof(vis));   //预处理
         vis[0][0]=1;       //标记A1已经经过
         scanf("%d%d",&p,&q);
         printf("Scenario #%d:\n",i);
-        if(DFS(p*q,1,0,0)){
+        if(dfs(p*q,1,0,0)){
             for(int j=0,tot=p*q;j<tot;j++)
                 printf("%c%d",way[j][0]+'A',way[j][1]+1);
             putchar('\n');
@@ -26,22 +39,4 @@ int main()
         if(i!=n)putchar('\n');
     }
     return 0;
-}
-
-int DFS(int tot,int rear,int x,int y)
-{
-    if(rear==tot)return 1;   //搜索成功
-    
-    else for(int i=0;i<8;i++){
-        int nx=x+dx[i],ny=y+dy[i];
-        if(nx>=0&&nx<q && ny>=0&&ny<p && !vis[nx][ny]){
-            vis[nx][ny]=1;      //标记为经过
-            if(DFS(tot,rear+1,nx,ny)){     //搜索下一个
-                way[rear][0]=nx,way[rear][1]=ny;
-                return 1;
-            }
-            vis[nx][ny]=0;   //搜索不成功，标记为未经过
-        }
-    }
-    return 0;   //搜索不成功
 }
--- 北大程序设计与算法专项课程/算法基础/深搜_棋盘问题.cpp
+++ 北大程序设计与算法专项课程/算法基础/深搜_棋盘问题.cpp
@@ -1,37 +1,34 @@
-#include<iostream>
-#include<cstdio>
-#include<cstring>
+// 这道题输入有点坑啊 用stdio的话要考虑很多换行符的事情。。还是cin保险
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
 using namespace std;
-int c,n,k,v[9];
-char map[9][9];
-bool a[9],b[9];
-int dfs(int x)
-{
-    for(int i=v[x-1];i<=n;i++)
-        for(int j=1;j<=n;j++)
-            if(!a[i]&&!b[j]&&map[i][j]=='#')
-            {
-                v[x]=i;
-                a[i]=1;
-                b[j]=1;
-                if(x==k) c++;
-                else dfs(x+1);
-                a[i]=0;
-                b[j]=0;
+
+int n, k, cnt;
+char arr[10][10]; int row[10], col[10], rec[10]; // rec[10]记录棋子放在第几行
+void dfs(int now) { // 当前排了几个棋子
+    for (int i = rec[now-1]; i <= n; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            if (arr[i][j] == '#' && !row[i] && !col[j]) {
+                rec[now] = i;
+                row[i] = col[j] = 1;
+                if (now == k) ++cnt;
+                else dfs(now+1);
+                row[i] = col[j] = 0;
             }
-    return 0;
-}
-int main()
-{
-    while(cin>>n>>k&&n!=-1&&k!=-1)
-    {
-        c=0;
-        memset(a,0,sizeof(a));
-        memset(b,0,sizeof(b));
-        for(int i=1;i<=n;i++)
-            for(int j=1;j<=n;j++)
-                cin>>map[i][j];
-        dfs(1);
-        cout<<c<<endl;
+        }
     }
+}
+
+int main() {
+    while (scanf("%d %d", &n, &k) != EOF) {
+        if (n == -1 && k == -1) break;
+        fill(row, row+10, 0); fill(col, col+10, 0);
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= n; ++j) cin >> arr[i][j];
+        }
+        cnt = 0; dfs(1);
+        printf("%d\n", cnt);
+    }
+    return 0;
 }
